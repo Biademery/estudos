fdebad12bacf86524d350716a15434408790ba17
Biademery <biademery@gmail.com>
2022-11-12 22:43:47 -0300

    add Aula06

---
+++ CJRM JavaScript/Módulo 01/Aula 06/app.js
@@ -0,0 +1,14 @@
+let age = 22;
+let currentYear = 2022;
+
+console.log(age, currentYear);
+
+age = 24;
+console.log(age);
+
+const points = 100;
+// points = 200; error
+console.log(points);
+
+var score = 75;
+console.log(score);
