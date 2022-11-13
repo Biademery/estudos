990a23f8f8e76ba37f24de0da65570373d696047
Biademery <biademery@gmail.com>
2022-11-13 17:48:12 -0300

    add Aula10

---
+++ CJRM JavaScript/Módulo 01/Aula 10/app.js
@@ -0,0 +1,31 @@
+// String
+console.log("Hello. world!");
+
+const email = "fulano@gmail.com";
+
+console.log(email);
+
+// Concatenação de strings
+const firstName = "Bia";
+const lastName = "D'Emery";
+
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// Acessando caracteres
+console.log(fullName[0]);
+
+// Comprimento de uma string
+console.log(fullName.length);
+
+// Metodos de string
+console.log(fullName.toUpperCase());
+
+const result = fullName.toLowerCase();
+
+console.log(result);
+
+const index = email.indexOf("@");
+
+console.log(index);
