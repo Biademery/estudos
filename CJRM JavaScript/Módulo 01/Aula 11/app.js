c1bf58b6511fd40f7db88859bfc3584ecd32daa7
Biademery <biademery@gmail.com>
2022-11-13 17:56:15 -0300

    add Aula11

---
+++ CJRM JavaScript/Módulo 01/Aula 11/app.js
@@ -0,0 +1,10 @@
+// Métodos de string
+const email = "laracroft@biademery.com";
+
+// const lastIndexOfA = email.lastIndexOf("a");
+// const emailslice = email.slice(0, 9);
+const emailReplace = email.replace("laracroft", "chun-li");
+
+// console.log(lastIndexOfA);
+// console.log(emailslice, email);
+console.log(emailReplace, email);
