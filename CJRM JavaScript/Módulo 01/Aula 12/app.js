bc41ee35d5b28f1e118d1fb42239e36559c43dcd
Biademery <biademery@gmail.com>
2022-11-13 18:08:41 -0300

    add Aula12

---
+++ CJRM JavaScript/Módulo 01/Aula 12/app.js
@@ -0,0 +1,26 @@
+//  inteiros decimais
+const radius = 10;
+const pi = 3.14;
+
+// operadores aritméticos
+const area = pi * radius ** 2;
+
+// ordem de operadores
+const crazyOperation = 5 * (10 - 3) ** 2;
+
+// operadores de incremento e decremento
+let postLikes = 10;
+
+// postLikes = postLikes + 1
+// postLikes++;
+// postLikes--
+
+// operadores addition, subtraction, multiplication e division
+// postLikes = postLikes + 10
+postLikes += 10;
+
+// NaN - not a number
+console.log(7 / "oi");
+
+// concatenação de string com número
+const likesMenssage = "O post tem" + postLikes + "likes";
