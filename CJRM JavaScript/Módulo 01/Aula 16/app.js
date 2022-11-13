7d8f2efda6b20a4ef6f61c63b1f5fe088253f068
Biademery <biademery@gmail.com>
2022-11-13 19:51:15 -0300

    change comment

--- CJRM JavaScript/Módulo 01/Aula 16/app.js
+++ CJRM JavaScript/Módulo 01/Aula 16/app.js
@@ -10,7 +10,7 @@ const randomArray = ["Parker", "Diana", 19, 18];
 const joinHeroes = heroes.join(", ");
 const indexOf25 = ages.indexOf("25");
 const moreHeroes = heroes.concat(["Superman", "Wolverine"]);
-const pushToHeroes = heroes.push(["Cyclops", "Hulk"]); // altera a string
+const pushToHeroes = heroes.push(["Cyclops", "Hulk"]); // altera a string original
 const popHeroes = heroes.pop(); //remove o ultimo item e retorna ele
 
 console.log(popHeroes);
