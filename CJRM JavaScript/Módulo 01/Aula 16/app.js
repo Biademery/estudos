e75e80f71712e2d64f7a8d539be809d2585cc2dd
Biademery <biademery@gmail.com>
2022-11-13 19:49:56 -0300

    add Aula16

---
+++ CJRM JavaScript/Módulo 01/Aula 16/app.js
@@ -0,0 +1,16 @@
+// Arrays
+let heroes = ["Batman", "Catwoman", "Iron Man"];
+
+heroes[2] = "Spider Man";
+
+const ages = [31, 22, 23, 40];
+const randomArray = ["Parker", "Diana", 19, 18];
+
+// Métodos de arrays
+const joinHeroes = heroes.join(", ");
+const indexOf25 = ages.indexOf("25");
+const moreHeroes = heroes.concat(["Superman", "Wolverine"]);
+const pushToHeroes = heroes.push(["Cyclops", "Hulk"]); // altera a string
+const popHeroes = heroes.pop(); //remove o ultimo item e retorna ele
+
+console.log(popHeroes);
