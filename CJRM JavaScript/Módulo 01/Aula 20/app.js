fb88457bbbd6edab7dbb899760a695fa4d6e1522
Biademery <biademery@gmail.com>
2022-11-14 15:35:53 -0300

    add Aula20

---
+++ CJRM JavaScript/Módulo 01/Aula 20/app.js
@@ -0,0 +1,26 @@
+// Booleans e comparações
+console.log(true, false, "true", "false");
+
+// Métodoas podem retornar booleans
+const email = "brucewayne@biademery.com";
+const includes = email.includes("@");
+const names = ["dio", "roger", "robert"];
+const arrayIncludes = names.includes("rob");
+
+// Operadores de comparação
+const age = 22;
+const name = "roger";
+
+// console.log(age == 22);
+// console.log(age == 24);
+// console.log(age != 24);
+// console.log(age > 20);
+// console.log(age < 22);
+// console.log(age <= 22);
+// console.log(age >= 22);
+
+console.log(name == "roger");
+console.log(name == "Roger");
+console.log(name > "bellinda");
+console.log(name > "Roger");
+console.log(name > "Bellinda");
