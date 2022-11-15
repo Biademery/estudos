2c55601d096d86f859d03889341a21d28f6af28d
Biademery <biademery@gmail.com>
2022-11-15 15:15:03 -0300

    add Aula03

---
+++ CJRM JavaScript/Módulo 02/Aula 03/app.js
@@ -0,0 +1,18 @@
+// if
+const age = 19;
+
+if (age > 18) {
+  console.log("Você tem mais de 18 anos");
+}
+
+const simpsons = ["Marge", "Homer", "Lisa", "Bart"];
+
+if (simpsons.length >= 5) {
+  console.log("o array tem bastante personagens");
+}
+
+const password = "oi123";
+
+if (password.length >= 8) {
+  console.log("essa senha tem 8 ou mais caracteres");
+}
