16fc91c70991cecfbff1a2497afa32b50113720d
Biademery <biademery@gmail.com>
2022-11-15 15:28:40 -0300

    add Aula03

---
+++ CJRM JavaScript/Módulo 02/Aula 03/app.js
@@ -0,0 +1,10 @@
+//  for loop
+const names = ["Linus", "Ada", "Bill"];
+
+for (let i = 0; i < names.length; i++) {
+  const HTMLTemplate = `<p>${names[i]}</p> `;
+
+  console.log(HTMLTemplate);
+}
+
+console.log("Loop concluído");
