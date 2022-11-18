2edb2fab90762cbac3fc099cbca0a5300dc95676
Biademery <biademery@gmail.com>
2022-11-18 17:08:11 -0300

    add Aula08

---
+++ CJRM JavaScript/Módulo 02/Aula 08/app.js
@@ -0,0 +1,10 @@
+// operadores lógicos - "OU" || e "E" &&
+const password = "oi123";
+
+if (password.length >= 12 && password.includes("1")) {
+  console.log("senha muito forte =)");
+} else if (password.length >= 8 || password.includes("1")) {
+  console.log("essa senha tem 8 ou mais caracteres");
+} else {
+  console.log("a senha deve conter 8 ou mais caracteres");
+}
