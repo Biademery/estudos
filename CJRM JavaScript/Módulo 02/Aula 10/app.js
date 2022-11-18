52bcf7896b73ad4df211c88c61d63b062e5b0c91
Biademery <biademery@gmail.com>
2022-11-18 18:03:06 -0300

    add Aula10

---
+++ CJRM JavaScript/Módulo 02/Aula 10/app.js
@@ -0,0 +1,9 @@
+// operadores lógicos not (!)
+let isUserLoggedIn = false;
+
+if (!isUserLoggedIn) {
+  console.log("Você precisa fazer login para continuar");
+}
+
+console.log(!true);
+console.log(!false);
