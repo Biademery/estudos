119141db29d0a6891f2a9f7112b16833ff066caf
Biademery <biademery@gmail.com>
2022-11-20 20:04:33 -0300

    add Aula10

---
+++ CJRM JavaScript/Módulo 03/Aula 10/app.js
@@ -0,0 +1,13 @@
+// utilizando callbacks
+
+const ul = document.querySelector("[data-js='ul']");
+
+const socialNetworks = ["youtube", "twitter", "instagram", "facebook"];
+
+let HTMLTemplate = "";
+
+socialNetworks.forEach((socialNetworks) => {
+  HTMLTemplate += `<li style="color: deeppink;">${socialNetworks}</li>`;
+});
+
+ul.innerHTML = HTMLTemplate;
