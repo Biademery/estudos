a4ad362a2943b98abf403f5a583c6c7b4907ac84
Biademery <biademery@gmail.com>
2022-11-20 19:47:49 -0300

    add Aula07

---
+++ CJRM JavaScript/Módulo 03/Aula 07/app.js
@@ -0,0 +1,14 @@
+// function tradicional
+// const double = function (number) {
+//   return number * 2;
+// };
+
+// arrow function
+const double = (number = 0) => {
+  return number * 2;
+};
+
+// const double = (number = 0) => number * 2;
+
+const result = double(3);
+console.log(showResult(result));
