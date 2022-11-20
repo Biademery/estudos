a2b91640a28c8e233a081e7db9db2e58aa9f2926
Biademery <biademery@gmail.com>
2022-11-20 19:58:25 -0300

    add Aula09

---
+++ CJRM JavaScript/Módulo 03/Aula 09/app.js
@@ -0,0 +1,18 @@
+// forEach e callbacks
+// const myFunc = (callback) => {
+//   const value = 77;
+
+//   callback(value);
+// };
+
+// myFunc((number) => {
+//     console.log(number)
+// });
+
+const socialNetworks = ["youtube", "twitter", "instagram", "facebook"];
+
+const logArrayInfo = (socialNetworks, index, array) => {
+  console.log(index, socialNetworks, array);
+};
+
+socialNetworks.forEach(logArrayInfo);
