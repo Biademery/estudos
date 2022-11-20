49563939e3ad045aed0d81d502d7daffe52804cf
Biademery <biademery@gmail.com>
2022-11-20 18:11:54 -0300

    add Aula03

---
+++ CJRM JavaScript/Módulo 03/Aula 03/app.js
@@ -0,0 +1,17 @@
+sayHi();
+sayHi();
+sayHi();
+
+// function declaration
+function sayHi() {
+  console.log("Oi");
+}
+
+// function expression
+const showFood = function () {
+  console.log("pizza");
+};
+
+showFood();
+showFood();
+showFood();
