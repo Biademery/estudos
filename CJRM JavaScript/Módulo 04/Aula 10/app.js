b27495b6a2178e79a77749f02dd34a509b4489f3
Biademery <biademery@gmail.com>
2022-12-17 00:42:18 -0300

    add Aula 10

---
+++ CJRM JavaScript/Módulo 04/Aula 10/app.js
@@ -0,0 +1,20 @@
+// objeto Math
+
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.E)
+
+const area = 7.7
+
+console.log(Math.round(area))
+console.log(Math.floor(area))
+console.log(Math.cell(area))
+console.log(Math.trunc(area))
+
+//números aleatórios
+
+const randomNumber = Math.random()
+
+console.log(randomNumber)
+
+console.log(Math.round(randomNumber * 100))
