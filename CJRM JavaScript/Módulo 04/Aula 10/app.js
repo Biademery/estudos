718408b474e24302489119a9008446b70c09c09b
Bia D'Emery <biademery@gmail.com>
2022-12-22 20:33:33 -0300

    changes

--- CJRM JavaScript/Módulo 04/Aula 10/app.js
+++ CJRM JavaScript/Módulo 04/Aula 10/app.js
@@ -1,20 +1,20 @@
 // objeto Math
 
-console.log(Math)
-console.log(Math.PI)
-console.log(Math.E)
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-const area = 7.7
+const area = 7.7;
 
-console.log(Math.round(area))
-console.log(Math.floor(area))
-console.log(Math.cell(area))
-console.log(Math.trunc(area))
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
-//números aleatórios
+// números aleatórios
 
-const randomNumber = Math.random()
+const randomNumber = Math.random();
 
-console.log(randomNumber)
+console.log(randomNumber);
 
-console.log(Math.round(randomNumber * 100))
+console.log(Math.round(randomNumber * 100));
