afc4d0de7d7f28bc3eb35deb3349189a57db1936
Bia D'Emery <biademery@gmail.com>
2022-12-27 16:11:18 -0300

    add Aula 07

---
+++ CJRM JavaScript/Módulo 05/Aula 07/app.js
@@ -0,0 +1,15 @@
+// obter um elemento através do ID
+// const title = document.getElementById('titile');
+
+// console.log(title);
+
+// obter elementos através do nome da classe
+
+// const errors = document.getElementsByClassName('error');
+
+// console.log(errors);
+
+// obter elementos através do nome da tag
+const paragraphs = document.getElementsByTagName('p');
+
+console.log(paragraphs);
