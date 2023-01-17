fccc3b0034e9a1a628f7bf0e47208c08dac8c159
Bia D'Emery <biademery@gmail.com>
2023-01-17 15:58:57 -0300

    Aula 02

---
+++ CJRM JavaScript/Módulo 10/Aula 02/app.js
@@ -0,0 +1,17 @@
+const present = new Date();
+
+console.log(present);
+
+console.log('getFullYear:', present.getFullYear());
+console.log('getMonth:', present.getMonth());
+console.log('getDate:', present.getDate());
+console.log('getDay:', present.getDay());
+console.log('getHours:', present.getHours());
+console.log('getMinutes:', present.getMinutes());
+console.log('getSeconds:', present.getSeconds());
+
+console.log('timestamp:', present.getTime());
+
+console.log('toDateString:', present.toDateString());
+console.log('toDateString:', present.toDateString());
+console.log('toLocalString:', present.toLocalString());
