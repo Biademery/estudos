87b70609ab33da3e41eef3d9782487e644437799
Bia D'Emery <biademery@gmail.com>
2023-01-17 17:02:05 -0300

    Aula 03

---
+++ CJRM JavaScript/Módulo 10/Aula 03/app.js
@@ -0,0 +1,24 @@
+const present = new Date();
+const past = new Date('May 1 2020 7:47:00');
+
+// console.log(present.getTime(), past.getTime());
+
+const difference = present.getTime() - past.getTime();
+console.log(difference);
+
+const seconds = Math.round(difference / 1000);
+console.log(seconds);
+
+const minutes = Math.round(seconds / 60);
+console.log(minutes);
+
+const hours = Math.round(minutes / 60);
+console.log(hours);
+
+const days = Math.round(hours / 24);
+console.log(days);
+
+console.log(`Escrito há ${days} dias`);
+
+const timestamp = 1675938474990;
+console.log(new Date(timestamp));
