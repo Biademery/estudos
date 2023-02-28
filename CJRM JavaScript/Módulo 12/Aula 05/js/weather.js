6af7d697e22e363d09ffdeab2ec41235d45dca7c
Biademery <biademery@gmail.com>
2023-02-28 17:14:34 -0300

    obtendo informações do clima nas cidades

--- CJRM JavaScript/Módulo 12/Aula 05/js/weather.js
+++ CJRM JavaScript/Módulo 12/Aula 05/js/weather.js
@@ -19,4 +19,21 @@ const getCityData = async cityName => {
   }
 };
 
+const getCityWeather = async cityName => {
+  try {
+    const { Key } = await getCityData(cityName);
+    const cityWeatherURL = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`;
+    const response = await fetch(cityWeatherURL);
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados');
+    }
+
+    const [cityWeatherData] = await response.json();
+    return cityWeatherData;
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`);
+  }
+};
+
 getCityData('São Paulo');
