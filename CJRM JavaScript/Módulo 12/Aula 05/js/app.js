3bf3c73436e938dc66375d1b9f2e86394d639df4
Biademery <biademery@gmail.com>
2023-03-01 12:20:06 -0300

    inserindo na interface

--- CJRM JavaScript/Módulo 12/Aula 05/js/app.js
+++ CJRM JavaScript/Módulo 12/Aula 05/js/app.js
@@ -1,11 +1,25 @@
 const cityForm = document.querySelector('[data-js="change-location"]');
+const cityNameContainer = document.querySelector('[data-js="city-name"]');
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]');
+const cityTemperatureContainer = document.querySelector(
+  '[data-js="city-temperature"]',
+);
+const cityCard = document.querySelector('[data-js="city-card"]');
 
 cityForm.addEventListener('subimt', async event => {
   event.preventDefault();
 
   const inputValue = event.target.city;
-  const [{ Key, localizedName }] = await getCityData(inputValue);
+  const [{ Key, LocalizedName }] = await getCityData(inputValue);
   const [{ WeatherText, Temperature }] = await getCityWeather(Key);
+
+  if (cityCard.classList.contains('d-none')) {
+    cityCard.classList.remove('d-none');
+  }
+
+  cityNameContainer.textContent = LocalizedName;
+  cityWeatherContainer.textContent = WeatherText;
+  cityTemperatureContainer.textContent = Temperature.Metric.Value;
 
   cityForm.reset();
 });
