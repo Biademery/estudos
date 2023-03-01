1d6ef9f2c0e45814147467919e6fe243c712555f
Biademery <biademery@gmail.com>
2023-03-01 14:38:05 -0300

    localStorage

---
+++ CJRM JavaScript/Módulo 13/Auula 04/app.js
@@ -0,0 +1,13 @@
+localStorage.setItem('name', 'Bia');
+localStorage.setItem('age', 23);
+
+const name = localStorage.getItem('name');
+const age = localStorage.getItem('age');
+
+// localStorage.setItem('name', 'Zé');
+// localStorage.setItem('age', 37);
+
+// name = localStorage.getItem('name');
+// age = localStorage.getItem('age');
+
+console.log(name, age);
