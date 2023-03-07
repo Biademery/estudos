b02b982e289b0755e7c42a78e2d9c44acd6ee3e9
Biademery <biademery@gmail.com>
2023-03-07 17:49:46 -0300

    watchers

--- Vue-Udemy/02. DOM Interactions with Vue/27. Data Binding + Event Binding = Two-Way Binding/app.js
+++ Vue-Udemy/02. DOM Interactions with Vue/27. Data Binding + Event Binding = Two-Way Binding/app.js
@@ -3,12 +3,14 @@ const app = Vue.createApp({
     return {
       counter: 10,
       name: '',
-      confirmedName: '',
+      // fullname: '',
+      lastName: '',
     };
   },
   methods: {
     resetButton() {
       this.name = '';
+      this.lastName = '';
     },
     addButton(number) {
       this.counter += number;
@@ -17,12 +19,37 @@ const app = Vue.createApp({
       this.counter -= number;
     },
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(() => {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // whenever name change this watch method will reexecut
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = `${value} ${this.lastName}`;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = `${this.name} ${value}`;
+    //   }
+    // },
+  },
   computed: {
     fullname() {
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return `${this.name} Demery`;
+      return `${this.name} ${this.lastName}`;
     },
   },
 });
