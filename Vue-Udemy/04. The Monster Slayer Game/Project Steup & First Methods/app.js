d9de3f97599cc8b248c590e4469a83607c893257
Biademery <biademery@gmail.com>
2023-03-08 18:32:45 -0300

    finishing the core functionality

--- Vue-Udemy/04. The Monster Slayer Game/Project Steup & First Methods/app.js
+++ Vue-Udemy/04. The Monster Slayer Game/Project Steup & First Methods/app.js
@@ -38,12 +38,27 @@ const app = Vue.createApp({
       }
       this.attackPlayer();
     },
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
+    surrender() {
+      this.winner = 'monster';
+    },
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: `${this.monsterHealth}%` };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: `${this.playerHealth}%` };
     },
     specialAttack() {
