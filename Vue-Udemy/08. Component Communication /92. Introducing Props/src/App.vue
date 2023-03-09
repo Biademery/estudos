9feb21b9312d1edd9b58c35fb9a4bc73b1973d55
Biademery <biademery@gmail.com>
2023-03-09 19:41:24 -0300

    adding components and connecting them

--- Vue-Udemy/08. Component Communication /92. Introducing Props/src/App.vue
+++ Vue-Udemy/08. Component Communication /92. Introducing Props/src/App.vue
@@ -3,10 +3,11 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact v-for="friend in friends" :key="friend.id" :id="friend.id" :name="friend.name"
         :phone-number="friend.phone" :email-address="friend.email" :is-favorite="friend.isFavorite"
-        @toggle-favorite="toggleFavoriteStatus"></friend-contact>
+        @toggle-favorite="toggleFavoriteStatus" @delete="deleteContact"></friend-contact>
     </ul>
   </section>
 </template>
@@ -39,6 +40,19 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId) {
+      this.friends = this.friends.filter((friend) => friend.id !== friendId);
     },
   },
 };
@@ -75,7 +89,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -108,4 +123,19 @@ header {
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
-</style>
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
+}</style>
