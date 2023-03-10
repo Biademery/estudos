790fa5b2deb5011a08b606ca301b46b903d87483
Biademery <biademery@gmail.com>
2023-03-10 14:00:51 -0300

    scoped slots

--- Vue-Udemy/09. Diving Deeper Into Components/109. Project Setup/src/App.vue
+++ Vue-Udemy/09. Diving Deeper Into Components/109. Project Setup/src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
-    <TheHeader />
+    <the-header></the-header>
+    <!-- <TheHeader /> -->
     <badge-list></badge-list>
-    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps['another-prop'] }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
 import BadgeList from './components/BadgeList.vue';
-
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     TheHeader,
-    UserInfo,
     BadgeList,
+    UserInfo,
+    CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Bia DEmery',
+        name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
